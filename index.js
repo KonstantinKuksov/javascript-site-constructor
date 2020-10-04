559602a0503b75859ec7f67c068ccf144345f7ea
KonstantinKuksov <61205290+KonstantinKuksov@users.noreply.github.com>
2020-10-04 23:08:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const model = [
+  { type: 'title', value: 'Hello, World from JS...' },
+  { type: 'text', value: 'This is some text from javascript' },
+  {
+    type: 'columns',
+    value: [
+      '1111111111111111111',
+      '2222222222222222222',
+      '3333333333333333333',
+    ],
+  },
+];
