18988f7353045f7c76b0e0b4cfb603f6208dfbe2
KonstantinKuksov <kkuksov@gmail.com>
2020-10-05 14:16:30 +0300

    refactor: npm init, use parcel, dividing the project into modules

---
+++ src/model.js
@@ -0,0 +1,13 @@
+export const model = [
+  { type: 'title', value: 'Hello, World from JS!!!!!' },
+  { type: 'text', value: 'This is some text from javascript' },
+  {
+    type: 'columns',
+    value: [
+      '1111111111111111111',
+      '2222222222222222222',
+      '3333333333333333333',
+    ],
+  },
+  { type: 'image', value: './assets/img.png' },
+];
