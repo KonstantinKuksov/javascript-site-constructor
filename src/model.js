5466a54da936b9ea7b2f039ad3290c47747784c6
KonstantinKuksov <kkuksov@gmail.com>
2020-10-05 16:09:30 +0300

    minor: save project files to open them on other PC

--- src/model.js
+++ src/model.js
@@ -8,13 +8,39 @@ export const model = [
       tag: 'h2',
       styles: {
         background: 'linear-gradient(to right, #ff0099, #493240)',
-        color: '#fff',
+        color: 'lightgreen',
         'text-align': 'center',
         padding: '1.5rem',
       },
     },
   },
-  { type: 'text', value: 'This is some text from javascript' },
+  {
+    type: 'image',
+    value: image,
+    options: {
+      styles: {
+        padding: '2rem 0',
+        display: 'flex',
+        'justify-content': 'center',
+      },
+      imageStyles: {
+        width: '500px',
+        height: 'auto',
+      },
+      alt: 'Это картинка',
+    },
+  },
+  {
+    type: 'text',
+    value: 'This is some text from javascript',
+    options: {
+      styles: {
+        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+        padding: '1rem',
+        'font-weight': 'bold',
+      },
+    },
+  },
   {
     type: 'columns',
     value: [
@@ -22,6 +48,13 @@ export const model = [
       '2222222222222222222',
       '3333333333333333333',
     ],
+    options: {
+      styles: {
+        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+        padding: '2rem',
+        color: '#fff',
+        'font-weight': 'bold',
+      },
+    },
   },
-  { type: 'image', value: image },
 ];
--- src/templates.js
+++ src/templates.js
@@ -1,22 +1,25 @@
 import { row, col, css } from './utils';
 
 function title(block) {
-  const {tag = 'h1', styles} = block.options;
+  const { tag = 'h1', styles } = block.options;
   return row(col(`<${tag}>${block.value}</${tag}>`), css(styles));
 }
 
 function text(block) {
-  return row(col(`<p>${block.value}</p>`));
+  const { styles } = block.options;
+  return row(col(`<p>${block.value}</p>`), css(styles));
 }
 
 function columns(block) {
+  const { styles } = block.options;
   const html = block.value.map(col);
 
-  return row(html.join(''));
+  return row(html.join(''), css(styles));
 }
 
 function image(block) {
-  return row(`<img src="${block.value}">`);
+  const { styles, imageStyles: is, alt = '' } = block.options;
+  return row(`<img src="${block.value}" style="${css(is)}" alt="${alt}">`, css(styles));
 }
 
 export const templates = {
