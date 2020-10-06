fe8a4f75f39d230a08b61dab1634d9743fbe1e42
KonstantinKuksov <kkuksov@gmail.com>
2020-10-06 12:10:54 +0300

    complete base functional of app

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,7 +1,60 @@
+import { row, col, css } from './../utils';
+
 class Block {
-  constructor(type, value, options) {
-    this.type = type;
+  constructor(value, options) {
     this.value = value;
     this.options = options;
+  }
+
+  toHTML() {
+    throw new Error('toHTML method must be implement!');
+  }
+}
+
+export class TitleBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+  }
+
+  toHTML() {
+    const { tag = 'h1', styles } = this.options;
+    return row(col(`<${tag}>${this.value}</${tag}>`), css(styles));
+  }
+}
+
+export class ImageBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+  }
+
+  toHTML() {
+    const { styles, imageStyles: is, alt = '' } = this.options;
+    return row(
+      `<img src="${this.value}" style="${css(is)}" alt="${alt}">`,
+      css(styles)
+    );
+  }
+}
+
+export class ColumnsBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+  }
+
+  toHTML() {
+    const { styles } = this.options;
+    const html = this.value.map(col);
+    return row(html.join(''), css(styles));
+  }
+}
+
+export class TextBlock extends Block {
+  constructor(value, options) {
+    super(value, options);
+  }
+
+  toHTML() {
+    const { styles } = this.options;
+    return row(col(`<p>${this.value}</p>`), css(styles));
   }
 }
--- src/model.js
+++ src/model.js
@@ -1,60 +1,50 @@
+import {
+  TitleBlock,
+  TextBlock,
+  ImageBlock,
+  ColumnsBlock,
+} from './classes/blocks';
 import image from './assets/img.png';
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Конструктор сайтов на JavaScript',
-    options: {
-      tag: 'h2',
-      styles: {
-        background: 'linear-gradient(to right, #ff0099, #493240)',
-        color: 'lightgreen',
-        'text-align': 'center',
-        padding: '1.5rem',
-      },
+  new TitleBlock('Так может выглядеть Ваш заголовок', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: 'white',
+      'text-align': 'center',
+      padding: '1.5rem',
     },
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        padding: '2rem 0',
-        display: 'flex',
-        'justify-content': 'center',
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto',
-      },
-      alt: 'Это картинка',
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center',
     },
-  },
-  {
-    type: 'text',
-    value: 'This is some text from javascript',
-    options: {
-      styles: {
-        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
-        padding: '1rem',
-        'font-weight': 'bold',
-      },
+    imageStyles: {
+      width: '500px',
+      height: 'auto',
     },
-  },
-  {
-    type: 'columns',
-    value: [
-      '1111111111111111111',
-      '2222222222222222222',
-      '3333333333333333333',
-    ],
-    options: {
+    alt: 'Это картинка',
+  }),
+  new TextBlock(`Это пример обычного текста: 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta at nisi dictum tristique. Sed consectetur blandit risus vel egestas. Sed eu eros lorem. Nunc sed dignissim ante, ac luctus tortor. Mauris quis tortor sit amet tellus fringilla fermentum. Curabitur eget aliquam ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros risus, blandit non felis ut, lobortis viverra mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam malesuada suscipit ultricies. Vivamus eu euismod leo, vel sodales arcu. Mauris vitae laoreet felis. Etiam felis elit, luctus ut vestibulum in, porta at risus. Nulla tortor nisi, vestibulum at elit at, fringilla ultrices sapien. Maecenas et magna ac nisl luctus ultricies.`, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold',
+    },
+  }),
+  new ColumnsBlock(
+    ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta at nisi dictum tristique. Sed consectetur blandit risus vel egestas. Sed eu eros lorem. Nunc sed dignissim ante, ac luctus tortor. Mauris quis tortor sit amet tellus fringilla fermentum. Curabitur eget aliquam ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros risus, blandit non felis ut, lobortis viverra mauris.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta at nisi dictum tristique. Sed consectetur blandit risus vel egestas. Sed eu eros lorem. Nunc sed dignissim ante, ac luctus tortor. Mauris quis tortor sit amet tellus fringilla fermentum. Curabitur eget aliquam ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros risus, blandit non felis ut, lobortis viverra mauris.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta at nisi dictum tristique. Sed consectetur blandit risus vel egestas. Sed eu eros lorem. Nunc sed dignissim ante, ac luctus tortor. Mauris quis tortor sit amet tellus fringilla fermentum. Curabitur eget aliquam ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros risus, blandit non felis ut, lobortis viverra mauris.'],
+    {
       styles: {
         background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
         padding: '2rem',
         color: '#fff',
         'font-weight': 'bold',
       },
-    },
-  },
+    }
+  ),
 ];
